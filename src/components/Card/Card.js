1d35d328fc94e10a1480e3bddf902ed45c1320be
rafalhen <rafalhen@users.noreply.github.com>
2021-05-23 17:03:41 +0200

    bugs in the sentences

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,13 @@ import styles from './Card.scss';
 import PropTypes from 'prop-types';
 
 const Card = props => (
-    <div className={styles.component}>
-        <h3>{props.title}</h3>
-    </div>
+  <div className={styles.component}>
+    <h3>{props.title}</h3>
+  </div>
 );
 
 Card.propTypes = {
-    title: PropTypes.string,
+  title: PropTypes.string,
 };
 
 export default Card;
